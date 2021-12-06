8b50d1792a96f6858c1af15c09ed66a190e58159
Abayomi <omosehin14@gmail.com>
2021-12-06 01:57:05 +0100

    initial commit

---
+++ Controllers/ExcelUploadController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using ReadExcelFile.Services;
+
+namespace ReadExcelFile.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ExcelUploadController : Controller
+    {
+        private readonly IReadFile file;
+
+        public ExcelUploadController(IReadFile file)
+        {
+            this.file = file;
+        }
+        [HttpPost]
+        public async Task<IActionResult> UploadAsync(IFormFile files)
+        {
+            var trs = await file.Readxlfile(files);
+           
+                return Ok("filePath");
+        }
+       
+    }
+}
---
+++ Services/ReadFile.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace ReadExcelFile.Services
+{
+    public class ReadingFile
+    {
+        public double item1 { get; set; }
+        public double item2 { get; set; }
+    }
+    public interface IReadFile
+    {
+        Task<string> Readxlfile(IFormFile file);
+    }
+    public class ReadFile : IReadFile
+    {
+        public async Task<string> Readxlfile(IFormFile file)
+        {
+            string line;
+            var res = new List<ReadingFile>();
+
+            using (var reader = new StreamReader(file.OpenReadStream()))
+            {
+                var parsedContentDisposition = ContentDispositionHeaderValue.Parse(file.ContentDisposition);
+                while (reader.Peek()>0)
+                {
+                    line = reader.ReadLine();
+
+                    res.Add(new ReadingFile
+                    {
+                        item1 = Convert.ToDouble( line.Split(',')[0]),
+                        item2 = Convert.ToDouble(line.Split(',')[1])
+                    });
+                }
+                
+            }
+
+            return "hello";
+        }
+    }
+}
